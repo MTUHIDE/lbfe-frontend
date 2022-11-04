ed965c88c9a949d560216fbf18fc93356c330309
Justin <jmilliman@serviceexpress.com/>
2022-11-03 20:01:33 -0400

    [feature] Breakout endpoints
    - moves all axios calls into functions
    
    NOW TO JUST REMOVE ALL THE OTHER CALLS.... :)

---
+++ src/js/endpoints.js
@@ -0,0 +1,114 @@
+import axios from 'axios'
+
+// Define the base URL for our request
+const baseUrl = "http://localhost:9010/api" // TODO - Load from ENV
+
+// Define the requestType we want. 
+// All routes have the same, so this lets us bulk edit if needed
+const getOne = '/?id='          // append id
+const getList = '/list'
+const postCreate = '/create'    // attach a body
+const postEdit = '/edit'        // attach a body
+const deleteDelete = '/delete'  // attach a body (only needs an id)
+
+
+// ********* APPOINTMENTS ********* 
+const appointmentUrl = baseUrl + '/appointment'
+
+// Call the getAllAppointments route in our backend
+// TODO - **Find a better way**
+export async function getAppointments(startDate, endDate) {
+    const requestUrl = appointmentUrl + getList + `?startDate=${startDate}&endDate=${endDate}`
+    return await axios.get(requestUrl)
+}
+
+// Retrieve specified Appointment by Id
+export async function getAppointmentById(id) {
+    const requestUrl = appointmentUrl + getOne + `${id}` // Intended to be manual, users can look up shit this way anyway
+    return await axios.get(requestUrl)
+}
+
+// Create a new appointment --> Assume appointment contains necessary info, route flags inproper usage
+export async function createAppointment(appointment) {
+    const requestUrl = appointmentUrl + postCreate
+    return await axios.post(requestUrl, appointment) // Pass appointment object
+}
+
+// Edit a given appointment --> Assume appointment contains necessary info, route flags inproper usage
+export async function editAppointment(appointment) {
+    const requestUrl = appointmentUrl + postEdit
+    return await axios.post(requestUrl, appointment) // Pass appointment object
+}
+
+export async function deleteAppointment(id) {
+    const requestUrl = appointmentUrl + deleteDelete
+    return await axios.delete(requestUrl, id) // Pass id in body
+}
+
+
+
+// ********* DRIVERS ************** 
+const driverUrl = baseUrl + '/driver'
+
+// Call the getAllDrivers route in our backend
+export async function getDrivers() {
+    const requestUrl = driverUrl + getList
+    return await axios.get(requestUrl)
+}
+
+// Retrieve specified Driver by Id
+export async function getDriverById(id) {
+    const requestUrl = driverUrl + getOne + `${id}` // Intended to be manual, users can look up shit this way anyway
+    return await axios.get(requestUrl)
+}
+
+// Create a new driver --> Assume driver object contains necessary info, route flags inproper usage
+export async function createDriver(driver) {
+    const requestUrl = driverUrl + postCreate
+    return await axios.post(requestUrl, driver) // Pass driver object
+}
+
+// Edit a given driver --> Assume driver object contains necessary info, route flags inproper usage
+export async function editDriver(driver) {
+    const requestUrl = driverUrl + postEdit
+    return await axios.post(requestUrl, driver) // Pass driver object
+}
+
+export async function deleteDriver(id) {
+    const requestUrl = driverUrl + deleteDelete
+    return await axios.delete(requestUrl, id) // Pass id in body
+}
+
+
+
+// ********* ELDERS *************** 
+const elderUrl = baseUrl + '/client'
+
+// Call the getAlElders route in our backend
+export async function getElders() {
+    const requestUrl = elderUrl + getList
+    return await axios.get(requestUrl)
+}
+
+// Retrieve specified Elder by Id
+export async function getElderById(id) {
+    const requestUrl = elderUrl + getOne + `${id}` // Intended to be manual, users can look up shit this way anyway
+    return await axios.get(requestUrl)
+}
+
+// Create a new elder --> Assume elder object contains necessary info, route flags inproper usage
+export async function createElder(elder) {
+    const requestUrl = elderUrl + postCreate
+    return await axios.post(requestUrl, elder) // Pass elder object
+}
+
+// Edit a given elder --> Assume elder object contains necessary info, route flags inproper usage
+export async function editElder(elder) {
+    const requestUrl = elderUrl + postEdit
+    return await axios.post(requestUrl, elder) // Pass elder object
+}
+
+export async function deleteElder(id) {
+    const requestUrl = elderUrl + deleteDelete
+    return await axios.delete(requestUrl, id) // Pass id in body
+}
--- src/main.js
+++ src/main.js
@@ -4,35 +4,20 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import Vuex from 'vuex';
 import router from "./router";
-// import createStore from "./store"; // TODO: Fix once Pinia is installed and working
 import Emitter from 'tiny-emitter';
-// import "mutationobserver-shim";
 import '@fortawesome/fontawesome-free/js/all';
-
-// I don't know what to do about this here....
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-import axios2 from 'axios'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
 const app = createApp(App);
 
-const store = new Vuex.Store({
-    state: {
-      app: null
-    },
-    mutations: {
-      app (state, app) {
-        state.app = app
-      }
-    },
-    modules: {}
-  });
-
 app.config.globalProperties.$msalInstance = {};
 // TODO: make the emitter a service
 app.config.globalProperties.$emitter = new Emitter();
 
-app.use(router).use(Vuex).use(store).use(VueAxios, { $axios: axios, axios2: axios2 }).mount('#app');
+app
+    .use(router)
+    // .use(store) // TODO - Fix once Pinia is implemented
+    .use(Vuex)
+    .mount('#app');
