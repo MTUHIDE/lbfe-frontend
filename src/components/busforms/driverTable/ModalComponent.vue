ea7d5452ec4157471fc851479021f6078267e826
dfbrink <dfbrink@mtu.edu>
2023-04-07 18:36:47 -0400

    frontend update - modals looked bad

---
+++ src/components/busforms/driverTable/ModalComponent.vue
@@ -0,0 +1,74 @@
+<template>
+  <transition name="modal-animation">
+    <div v-show="modalActive" class="modal">
+      <transition name="modal-animation-inner">
+        <div v-show="modalActive" class="modal-inner">
+          <!-- Modal Content -->
+          <slot />
+        </div>
+      </transition>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "ModalComponent",
+  props: ["modalActive"],
+
+  // Expose our close function
+  setup(props, { emit }) {
+    const close = () => {
+      emit("close");
+    };
+    return { close };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.modal-animation-enter-active,
+.modal-animation-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-animation-enter-from,
+.modal-animation-leave-to {
+  opacity: 0;
+}
+.modal-animation-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+.modal-animation-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-animation-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+.modal-animation-inner-leave-to {
+  transform: scale(0.8);
+}
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(156, 156, 156, 0.7);
+  border: hidden;
+
+  .modal-inner {
+    border-radius: 9px;
+    border: hidden;
+    border-color: #fff;
+    position: relative;
+    max-width: 575px;
+    width: 45em; //this is the width of the white box
+    background-color: #fff;
+    padding: 8px 8px;
+  }
+}
+</style>
