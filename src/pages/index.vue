47bae17741123657da67dca67852b0dd554546e7
Siddhartha Regmi <regmisiddhartha01@gmail.com>
2025-03-05 21:20:40 -0500

    initial routing

---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<template lang="">
+  <div>
+    <h1>Welcome to LBFE</h1>
+
+    <RouterLink to="/Calendar"> Go to Calendar</RouterLink>
+  </div>
+</template>
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import VueRouter from 'unplugin-vue-router/vite'
+
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,6 +10,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({}),
     vue(),
     vueDevTools(),
   ],
