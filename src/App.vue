9448acd38eccff5b2bc60b45856e99e6bb3a0bb2
spencerPhillips <spencerp@mtu.edu>
2025-03-05 20:47:56 -0500

    trying to remaking project, is VERY EXPERIMENTAL

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,85 @@
-<script>
-import NavBar from "./components/navbar/Navbar.vue"
-require("./css/main-style.css");
-
-export default {
-  name: "App",
-  components: { NavBar },
-};
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div id="app">
-    <NavBar v-if="!$route.meta.hideNavbar"/>
-    <div class="main-content">
-      <router-view></router-view>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
-    <footer>Brought to you by HIDE (MTU)</footer>
-  </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<style>
-html,
-body {
-  min-height: 100vh;
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-#app {
-  font-family: Avenir, Helvetica, Roboto, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-label {
-  float: left;
-}
-
-.main-content {
-  min-height: 100%;
-  margin-top: 100px;
-  margin-bottom: -32px;
-}
-
-.main-content:after {
-  content: "";
+.logo {
   display: block;
+  margin: 0 auto 2rem;
 }
 
-footer,
-.main-content:after {
-  height: 32px;
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
 }
 
-.modal-backdrop.show:nth-of-type(odd) {
-    z-index: 1061 !important;
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
--- src/components/busforms/UpdateForm.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <h1>This will eventually be the update log page :)</h1>
-</template>
-
-<script>
-export default {};
-</script>
